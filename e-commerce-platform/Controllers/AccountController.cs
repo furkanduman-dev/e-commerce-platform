3e1dc40ea05a5a518d3c4ab49a996636e6ed212b
RebelliousCoder <149897434+RebelliousCoder@users.noreply.github.com>
2026-02-23 00:05:48 +0300

    last

--- e-commerce-platform/Controllers/AccountController.cs
+++ e-commerce-platform/Controllers/AccountController.cs
@@ -48,11 +48,13 @@ public class AccountController : Controller
             if (result.Succeeded)
             {
                 return RedirectToAction("Index", "Home");
+
             }
 
             foreach (var error in result.Errors)
             {
                 ModelState.AddModelError("", error.Description);
+
             }
         }
         return View(createModel);
