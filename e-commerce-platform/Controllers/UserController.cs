c674bfddbcd56c89e507016b8dc25d0cb5bf34f0
RebelliousCoder <149897434+RebelliousCoder@users.noreply.github.com>
2026-02-25 20:44:32 +0300

    Userfixed

--- e-commerce-platform/Controllers/UserController.cs
+++ e-commerce-platform/Controllers/UserController.cs
@@ -1,4 +1,8 @@
+using System.Threading.Tasks;
+using e_commerce_platform.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace e_commerce_platform.Controllers;
 
@@ -6,8 +10,27 @@ namespace e_commerce_platform.Controllers;
 public class UserController : Controller
 {
 
-    public ActionResult Index()
+    private UserManager<AppUser> _userManager;
+
+    private RoleManager<AppRole> _roleManager;
+    private readonly DataContext _context;
+
+    public UserController(DataContext context, UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
     {
-        return View();
+        _userManager = userManager;
+        _context = context;
+        _roleManager = roleManager;
+    }
+
+    public async Task<ActionResult> Index(string role)
+    {
+        ViewBag.Roles = new SelectList(_roleManager.Roles, "Name", "Name", role);
+
+        if (!string.IsNullOrEmpty(role))
+        {
+            return View(await _userManager.GetUsersInRoleAsync(role));
+        }
+
+        return View(_userManager.Users);
     }
 }
