1e4a982191d93509a8b19c85e52914f9d536d1fc
RebelliousCoder <149897434+RebelliousCoder@users.noreply.github.com>
2026-03-06 20:59:59 +0300

    AdminController

--- e-commerce-platform/Controllers/AdminController.cs
+++ e-commerce-platform/Controllers/AdminController.cs
@@ -1,3 +1,4 @@
+using e_commerce_platform.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,9 +7,15 @@ namespace e_commerce_platform.Controllers;
 
 public class AdminController : Controller
 {
+    private readonly DataContext _context;
 
-    public ActionResult Index()
+    public AdminController(DataContext context)
     {
+        _context = context;
+    }
+    public ActionResult Index(ProductGetModel model)
+    {
+
         return View();
 
     }
