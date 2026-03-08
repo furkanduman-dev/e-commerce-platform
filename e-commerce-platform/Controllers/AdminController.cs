8147007a09127c6893d9b3098d2414ee7b510831
RebelliousCoder <149897434+RebelliousCoder@users.noreply.github.com>
2026-03-08 20:16:00 +0300

    AdminIndex

--- e-commerce-platform/Controllers/AdminController.cs
+++ e-commerce-platform/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 using e_commerce_platform.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace e_commerce_platform.Controllers;
 
@@ -13,10 +14,19 @@ public class AdminController : Controller
     {
         _context = context;
     }
-    public ActionResult Index(ProductGetModel model)
+    public ActionResult Index()
     {
+        var orders = _context.Orders
+        .Include(o => o.OrderItems)
+        .ThenInclude(oi => oi.Product)
+        .ToList();
 
-        return View();
+        ViewBag.ProductCount = _context.Products.Count();
+        ViewBag.TotalPrice = _context.Orders.Sum(o => o.ToplamFiyat);
+        ViewBag.TotalOrder = _context.Orders.Count();
+        ViewBag.TotallUser = _context.Users.Count();
+
+        return View(orders);
 
     }
 }
